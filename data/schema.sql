cec5fc2d222e0eb18157945f1963cfdc32cb160e
blushi <marie.gauthier63@gmail.com>
2020-04-01 13:53:01 +0200

    #171443795 Add psql function issue_credits

--- data/schema.sql
+++ data/schema.sql
@@ -1157,6 +1157,111 @@ $$;
 
 
 --
+-- Name: credit_vintages; Type: TABLE; Schema: app_public; Owner: -
+--
+
+CREATE TABLE app_public.credit_vintages (
+    id uuid DEFAULT public.uuid_generate_v1() NOT NULL,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    credit_class_id uuid,
+    project_id uuid,
+    issuer_id uuid,
+    units integer,
+    initial_distribution jsonb
+);
+
+
+--
+-- Name: issue_credits(uuid, integer, jsonb); Type: FUNCTION; Schema: app_public; Owner: -
+--
+
+CREATE FUNCTION app_public.issue_credits(project_id uuid, units integer, initial_distribution jsonb) RETURNS app_public.credit_vintages
+    LANGUAGE plpgsql STRICT SECURITY DEFINER
+    SET search_path TO 'pg_catalog', 'public', 'pg_temp'
+    AS $$
+declare
+  -- v_credit_class_id uuid;
+  v_issuer_wallet_id uuid;
+  v_credit_class_issuer_id uuid;
+  v_issuee_id uuid;
+  v_credit_vintage app_public.credit_vintages;
+  v_project app_public.projects;
+  v_key text;
+  v_value numeric;
+begin
+  if app_public.current_user_id() is null then
+    raise exception 'You must log in to issue credits' using errcode = 'LOGIN';
+  end if;
+
+  -- find project
+  select * into v_project
+  from app_public.projects
+  where id = project_id;
+
+  if v_project.id is null then
+    raise exception 'Project not found' using errcode = 'NTFND';
+  end if;
+
+  -- get current user (issuer)'s wallet id
+  select wallet_id into v_issuer_wallet_id
+  from app_public.users
+  where id = app_public.current_user_id();
+
+  if v_issuer_wallet_id is null then
+    raise exception 'Wallet is required' using errcode = 'NTFND';
+  end if;
+
+  -- verify current user is allowed to issue credits for this credit class
+  select id into v_credit_class_issuer_id from app_public.credit_class_issuers
+  where credit_class_id = v_project.credit_class_id and issuer_id = v_issuer_wallet_id;
+
+  if v_credit_class_issuer_id is null then
+    raise exception 'User not allowed to issue credits for this project' using errcode = 'DNIED';
+  end if;
+
+  -- create credit vintage
+  insert into app_public.credit_vintages (credit_class_id, project_id, issuer_id, units, initial_distribution)
+    values(v_project.credit_class_id, project_id, v_issuer_wallet_id, units, initial_distribution)
+    returning * into v_credit_vintage;
+
+  -- create account balances
+  for v_key, v_value IN
+     select * from jsonb_each_text(initial_distribution)
+  loop
+     -- raise notice '%: %', v_key, v_value;
+     if v_value != 0 then
+      if v_key = 'projectDeveloper' then
+        if v_project.developer_id is null then
+          raise exception 'Project does not have any project developer' using errcode = 'NTFND';
+        end if;
+        v_issuee_id := v_project.developer_id;
+      end if;
+
+      if v_key = 'landOwner' then
+        if v_project.land_owner_id is null then
+          raise exception 'Project does not have any land owner' using errcode = 'NTFND';
+        end if;
+        v_issuee_id := v_project.land_owner_id;
+      end if;
+
+      if v_key = 'landSteward' then
+        if v_project.steward_id is null then
+          raise exception 'Project does not have any land steward' using errcode = 'NTFND';
+        end if;
+        v_issuee_id := v_project.steward_id;
+      end if;
+
+      insert into app_public.account_balances (credit_vintage_id, wallet_id, liquid_balance, burnt_balance)
+        values (v_project.credit_vintage_id, v_issuee_id, v_value * units , 0);
+    end if;
+  end loop;
+
+  return v_credit_vintage;
+end;
+$$;
+
+
+--
 -- Name: logout(); Type: FUNCTION; Schema: app_public; Owner: -
 --
 
@@ -1898,21 +2003,6 @@ CREATE TABLE app_public.credit_classes (
     updated_at timestamp with time zone DEFAULT now() NOT NULL,
     designer_id uuid,
     methodology_id uuid NOT NULL
-);
-
-
---
--- Name: credit_vintages; Type: TABLE; Schema: app_public; Owner: -
---
-
-CREATE TABLE app_public.credit_vintages (
-    id uuid DEFAULT public.uuid_generate_v1() NOT NULL,
-    created_at timestamp with time zone DEFAULT now() NOT NULL,
-    credit_class_id uuid,
-    project_id uuid,
-    issuer_id uuid,
-    units integer,
-    initial_distribution jsonb
 );
 
 
@@ -3365,6 +3455,56 @@ GRANT ALL ON FUNCTION app_public.invite_to_organization(organization_id uuid, us
 
 
 --
+-- Name: TABLE credit_vintages; Type: ACL; Schema: app_public; Owner: -
+--
+
+GRANT SELECT,DELETE ON TABLE app_public.credit_vintages TO regen_registry_visitor;
+
+
+--
+-- Name: COLUMN credit_vintages.credit_class_id; Type: ACL; Schema: app_public; Owner: -
+--
+
+GRANT INSERT(credit_class_id),UPDATE(credit_class_id) ON TABLE app_public.credit_vintages TO regen_registry_visitor;
+
+
+--
+-- Name: COLUMN credit_vintages.project_id; Type: ACL; Schema: app_public; Owner: -
+--
+
+GRANT INSERT(project_id),UPDATE(project_id) ON TABLE app_public.credit_vintages TO regen_registry_visitor;
+
+
+--
+-- Name: COLUMN credit_vintages.issuer_id; Type: ACL; Schema: app_public; Owner: -
+--
+
+GRANT INSERT(issuer_id),UPDATE(issuer_id) ON TABLE app_public.credit_vintages TO regen_registry_visitor;
+
+
+--
+-- Name: COLUMN credit_vintages.units; Type: ACL; Schema: app_public; Owner: -
+--
+
+GRANT INSERT(units),UPDATE(units) ON TABLE app_public.credit_vintages TO regen_registry_visitor;
+
+
+--
+-- Name: COLUMN credit_vintages.initial_distribution; Type: ACL; Schema: app_public; Owner: -
+--
+
+GRANT INSERT(initial_distribution),UPDATE(initial_distribution) ON TABLE app_public.credit_vintages TO regen_registry_visitor;
+
+
+--
+-- Name: FUNCTION issue_credits(project_id uuid, units integer, initial_distribution jsonb); Type: ACL; Schema: app_public; Owner: -
+--
+
+REVOKE ALL ON FUNCTION app_public.issue_credits(project_id uuid, units integer, initial_distribution jsonb) FROM PUBLIC;
+GRANT ALL ON FUNCTION app_public.issue_credits(project_id uuid, units integer, initial_distribution jsonb) TO regen_registry_visitor;
+
+
+--
 -- Name: FUNCTION logout(); Type: ACL; Schema: app_public; Owner: -
 --
 
@@ -3651,48 +3791,6 @@ GRANT INSERT(designer_id),UPDATE(designer_id) ON TABLE app_public.credit_classes
 --
 
 GRANT INSERT(methodology_id),UPDATE(methodology_id) ON TABLE app_public.credit_classes TO regen_registry_visitor;
-
-
---
--- Name: TABLE credit_vintages; Type: ACL; Schema: app_public; Owner: -
---
-
-GRANT SELECT,DELETE ON TABLE app_public.credit_vintages TO regen_registry_visitor;
-
-
---
--- Name: COLUMN credit_vintages.credit_class_id; Type: ACL; Schema: app_public; Owner: -
---
-
-GRANT INSERT(credit_class_id),UPDATE(credit_class_id) ON TABLE app_public.credit_vintages TO regen_registry_visitor;
-
-
---
--- Name: COLUMN credit_vintages.project_id; Type: ACL; Schema: app_public; Owner: -
---
-
-GRANT INSERT(project_id),UPDATE(project_id) ON TABLE app_public.credit_vintages TO regen_registry_visitor;
-
-
---
--- Name: COLUMN credit_vintages.issuer_id; Type: ACL; Schema: app_public; Owner: -
---
-
-GRANT INSERT(issuer_id),UPDATE(issuer_id) ON TABLE app_public.credit_vintages TO regen_registry_visitor;
-
-
---
--- Name: COLUMN credit_vintages.units; Type: ACL; Schema: app_public; Owner: -
---
-
-GRANT INSERT(units),UPDATE(units) ON TABLE app_public.credit_vintages TO regen_registry_visitor;
-
-
---
--- Name: COLUMN credit_vintages.initial_distribution; Type: ACL; Schema: app_public; Owner: -
---
-
-GRANT INSERT(initial_distribution),UPDATE(initial_distribution) ON TABLE app_public.credit_vintages TO regen_registry_visitor;
 
 
 --
